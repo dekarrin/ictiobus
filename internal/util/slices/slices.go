c365ddac9726de2a9a367ad8c47e9733a419cdfe
Rebecca Nelson <dekarrin.irl@gmail.com>
2023-03-04 11:38:00 -0600

    EOL

--- internal/util/slices/slices.go
+++ internal/util/slices/slices.go
@@ -1,6 +1,8 @@
 // Package slices contains functions useful for operating on slices.
 package slices
 
+import "sort"
+
 // In returns whether s is present in the given slice by checking each item
 // in order.
 func In[V comparable](s V, slice []V) bool {
@@ -76,4 +78,40 @@ func HasPrefix[T comparable](sl []T, prefix []T) bool {
 	}
 
 	return true
+}
+
+type sorter[E any] struct {
+	src []E
+	lt  func(left, right E) bool
+}
+
+func (s sorter[E]) Len() int {
+	return len(s.src)
+}
+
+func (s sorter[E]) Swap(i, j int) {
+	s.src[i], s.src[j] = s.src[j], s.src[i]
+}
+
+func (s sorter[E]) Less(i, j int) bool {
+	return s.lt(s.src[i], s.src[j])
+}
+
+// SortBy takes the items and uses the provided function to sort the list. The
+// function should return true if left is less than (comes before) right.
+//
+// items will not be modified.
+func SortBy[E any](items []E, lt func(left E, right E) bool) []E {
+	if len(items) == 0 || lt == nil {
+		return items
+	}
+
+	s := sorter[E]{
+		src: make([]E, len(items)),
+		lt:  lt,
+	}
+
+	copy(s.src, items)
+	sort.Sort(s)
+	return s.src
 }
---
+++ internal/util/stack/stack.go
@@ -0,0 +1,125 @@
+// Package stack provies a stack structure based off of a Slice.
+package stack
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+// Stack is a stack. It is backed by a slice where the left-most position is the
+// top of the stack. The zero-value for the stack is ready to use.
+type Stack[E any] struct {
+	Of []E
+}
+
+// Push pushes value onto the stack.
+func (s *Stack[E]) Push(v E) {
+	if s == nil {
+		panic("push to nil stack")
+	}
+	s.Of = append([]E{v}, s.Of...)
+}
+
+// Pop pops value off of the stack. If the stack is empty, panics.
+func (s *Stack[E]) Pop() E {
+	if s == nil {
+		panic("pop of nil stack")
+	}
+	if len(s.Of) == 0 {
+		panic("pop of empty stack")
+	}
+
+	v := s.Of[0]
+	newVals := make([]E, len(s.Of)-1)
+	copy(newVals, s.Of[1:])
+	s.Of = newVals
+	return v
+}
+
+// Peek checks the value at the top of the stack. If the stack is empty, panics.
+func (s Stack[E]) Peek() E {
+	if len(s.Of) == 0 {
+		panic("peek of empty stack")
+	}
+	return s.Of[0]
+}
+
+// PeekAt checks the value at the given position of the stack. If the stack is
+// not that long, panics.
+func (s Stack[E]) PeekAt(p int) E {
+	if p >= len(s.Of) {
+		panic(fmt.Sprintf("stack index out of range: %d", p))
+	}
+	return s.Of[p]
+}
+
+// Len returns the length of the stack.
+func (s Stack[E]) Len() int {
+	return len(s.Of)
+}
+
+// Empty returns whether the stack is empty.
+func (s Stack[E]) Empty() bool {
+	return len(s.Of) == 0
+}
+
+// String shows the contents of the stack as a simple slice.
+func (s Stack[E]) String() string {
+	var sb strings.Builder
+
+	sb.WriteString("Stack[")
+	for i := range s.Of {
+		sb.WriteString(fmt.Sprintf("%v", s.Of[i]))
+		if i+1 < len(s.Of) {
+			sb.WriteRune(',')
+			sb.WriteRune(' ')
+		}
+	}
+	sb.WriteRune(']')
+	return sb.String()
+}
+
+// Equal returns whether two stacks have exactly the same contents in the same
+// order. If anything other than a Stack[E], *Stack[E], []E, or *[]E is passed
+// in, they will not be considered equal.
+//
+// TODO: these docs are kind of lies, fix it
+//
+// A Stack[E] *is* considered equal to a *[]E or []E that has the same contents
+// as the Stack in the same order.
+//
+// This does NOT do Equal on the individual items, but rather a simple equality
+// check. To do full Equal on everything, use EqualSlices on the Ofs of the
+// stacks.
+func (s Stack[E]) Equal(o any) bool {
+	other, ok := o.(Stack[E])
+	if !ok {
+		// also okay if its the pointer value, as long as its non-nil
+		otherPtr, ok := o.(*Stack[E])
+		if !ok {
+			// also okay if it's a slice
+			otherSlice, ok := o.([]E)
+
+			if !ok {
+				// also okay if it's a ptr to slice
+				otherSlicePtr, ok := o.(*[]E)
+				if !ok {
+					return false
+				} else if otherSlicePtr == nil {
+					return false
+				} else {
+					other = Stack[E]{Of: *otherSlicePtr}
+				}
+			} else {
+				other = Stack[E]{Of: otherSlice}
+			}
+		} else if otherPtr == nil {
+			return false
+		} else {
+			other = *otherPtr
+		}
+	}
+
+	return reflect.DeepEqual(s.Of, other.Of)
+}
