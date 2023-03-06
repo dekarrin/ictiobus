364a219bdc36584f6181d45573011be2271132d7
dekarrin <dekarrin.irl@gmail.com>
2023-03-06 10:23:44 -0600

    #3: started unit tests for dec and enc int

--- internal/decbin/basictypes_test.go
+++ internal/decbin/basictypes_test.go
@@ -102,3 +102,114 @@ func Test_DecBool(t *testing.T) {
 		})
 	}
 }
+
+func Test_EncInt(t *testing.T) {
+	testCases := []struct {
+		name   string
+		input  int
+		expect []byte
+	}{
+		{
+			name:   "zero",
+			input:  0,
+			expect: []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+		},
+		{
+			name:   "1",
+			input:  1,
+			expect: []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01},
+		},
+		{
+			name:   "256",
+			input:  256,
+			expect: []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00},
+		},
+		{
+			name:   "328493",
+			input:  328493,
+			expect: []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x05, 0x03, 0x2d},
+		},
+		{
+			name:   "413",
+			input:  413,
+			expect: []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x9d},
+		},
+		{
+			name:   "-413",
+			input:  -413,
+			expect: []byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xfe, 0x63},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert := assert.New(t)
+
+			actual := EncInt(tc.input)
+
+			assert.Equal(tc.expect, actual)
+		})
+	}
+}
+
+func Test_DecInt(t *testing.T) {
+	testCases := []struct {
+		name        string
+		input       []byte
+		expectValue int
+		expectRead  int
+		expectError bool
+	}{
+		{
+			name:        "0 from exact value",
+			input:       []byte{0x01},
+			expectValue: 0,
+			expectRead:  1,
+		},
+
+		{
+			name:        "1 from exact value",
+			input:       []byte{0x01},
+			expectValue: 0,
+			expectRead:  1,
+		},
+
+		{
+			name:        "-1 from exact value",
+			input:       []byte{0x01},
+			expectValue: 0,
+			expectRead:  1,
+		},
+
+		{
+			name:        "413 from exact value",
+			input:       []byte{0x01},
+			expectValue: 0,
+			expectRead:  1,
+		},
+		{
+			name:        "-413413413 from exact value",
+			input:       []byte{0x01},
+			expectValue: -413413413,
+			expectRead:  1,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert := assert.New(t)
+
+			actualValue, actualRead, err := DecBool(tc.input)
+			if tc.expectError {
+				if !assert.Error(err) {
+					return
+				}
+			} else if !assert.NoError(err) {
+				return
+			}
+
+			assert.Equal(tc.expectValue, actualValue)
+			assert.Equal(tc.expectRead, actualRead, "num read bytes does not match expected")
+		})
+	}
+}
