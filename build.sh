957e8526020a8516298e3abe3d656683ac389e0e
dekarrin <dekarrin.irl@gmail.com>
2023-03-24 14:32:42 -0500

    #14: first attempt at ictcc executable

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#!/bin/bash
+
+set -eo pipefail
+
+if ! which go >/dev/null 2>&1
+then
+  echo "The Go language compiler does not appear to be installed" >&2
+  echo "Install Go from https://go.dev/doc/install and try again" >&2
+  exit 1
+fi
+
+cd "$(dirname "$0")"
+
+ext=
+for_windows=
+[ "$(go env GOOS)" = "windows" ] && for_windows=1 && ext=".exe"
+
+env CGO_ENABLED=0 go build -o ictcc$ext cmd/ictcc/main.go
