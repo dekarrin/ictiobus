9611ac432577b9419098ba029758e6d6bad25fee
dekarrin <dekarrin.irl@gmail.com>
2023-04-04 09:27:59 -0500

    #16: sdts verify spec does not refer to invalid attribute

---
+++ fishigen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# shortcut for running ictcc on fishi.md for when we build a new ictcc bin
+
+./ictcc -clr \
+	-ir '[]github.com/dekarrin/ictiobus/fishi/syntax.Block' \
+	-dest testout \
+	-l fishi -lang-ver 1.0.0 \
+	-hooks fishi/syntax \
+	fishi.md
+
