b87f8a0045964f240a4c6cd06ad464c5b6409cdd
dekarrin <dekarrin.irl@gmail.com>
2023-04-17 06:45:07 -0500

    #16: make genfishi.sh use --dev

--- genfishi.sh
+++ genfishi.sh
@@ -2,11 +2,14 @@
 
 # shortcut for running ictcc on fishi.md for when we build a new ictcc bin
 
+cd "$(dirname "$0")"
+
 ./ictcc --clr \
 	--ir '[]github.com/dekarrin/ictiobus/fishi/syntax.Block' \
 	--dest .testout \
 	-l FISHI -v 1.0.0 \
 	--hooks fishi/syntax \
 	-d fishic \
+	--dev \
 	-f fishi/format \
 	fishi.md "$@"
