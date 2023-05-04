b47f8041b496fb264aaa0cefa17857779cdd8871
Deka Jello <dekarrin.irl@gmail.com>
2023-05-04 09:00:52 -0500

    #110: add new test for simplemath for bad sdts

---
+++ test/simplemath-bad-sdts/gen.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Runs ictcc to generate a diagnostics binary 'smc' for simplemath.md. Execute
+# this script from the root of the project after building ictcc.
+
+script_path="$(dirname "$0")"
+
+./ictcc --clr \
+	--ir 'int' \
+	-l SimpleMath -v 1.0.0 \
+	--hooks "$script_path/hooks" \
+	-d "$script_path/smc" \
+	--dev \
+	-n \
+	"$script_path/simplemath.md" "$@"
