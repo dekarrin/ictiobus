bdb3c1abc6cb4182d97ebd5b2da730ba11cb1512
Deka Jello <dekarrin.irl@gmail.com>
2023-05-04 09:18:55 -0500

    #110: simple math gen update

---
+++ test/simplemath/tests/01-normal-gen/exec.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+script_path="$(cd "$(dirname "$0")" >/dev/null ; pwd -P)"
+simplemath_path="$(cd "$script_path/../.." >/dev/null ; pwd -P)"
+
+./ictcc --clr \
+	--ir 'int' \
+	-l SimpleMath -v 1.0.0 \
+	-d "$script_path/testdiag" \
+    --hooks "$script_path/hooks" \
+	--dev \
+	-n \
+	"$simplemath_path/simplemath.md"
+
+
