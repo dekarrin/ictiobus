f5d80766690cee01a6256c326ad611e83a443198
Deka Jello <dekarrin.irl@gmail.com>
2023-05-04 09:38:04 -0500

    #110: add second test rearrange

--- test/02-sm-disconnected-sdts/exec.sh
+++ test/02-sm-disconnected-sdts/exec.sh
@@ -11,12 +11,3 @@ script_path="$(cd "$(dirname "$0")" >/dev/null ; pwd -P)"
 	-n \
 	"$script_path/simplemath.md" || { echo "FAIL" >&2 ; exit 1 ; }
 
-echo -n "2+3 = "
-"$script_path"/testdiag -C "2+3" || { echo "FAIL" >&2 ; exit 1 ; }
-
-echo -n "2 = "
-"$script_path"/testdiag -C "2"   || { echo "FAIL" >&2 ; exit 1 ; }
-
-echo -n "2*3 = "
-"$script_path"/testdiag -C "2*3" || { echo "FAIL" >&2 ; exit 1 ; }
-
