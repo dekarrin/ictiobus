85ea92ff522934f1c9b696ab31976360ad491ef1
Rebecca Nelson <dekarrin.irl@gmail.com>
2023-05-05 17:11:19 -0500

    Use new -D instead of --dfa in dfa tests. As a treat ::::)

--- tests/05-output-dfa/exec.sh
+++ tests/05-output-dfa/exec.sh
@@ -5,31 +5,27 @@ script_path="$(cd "$(dirname "$0")" >/dev/null ; pwd -P)"
 echo "[1/4] Output CLR(1) DFA:"
 ./ictcc --clr \
 	-l SimpleMath -v 1.0.0 \
-	-nq \
-	--dfa \
+	-nqD \
 	"$script_path/simplemath.md" || { echo "FAIL" >&2 ; exit 1 ; }
 echo "(done)"
 
 echo "[2/4] Output SLR(1) DFA:"
 ./ictcc --slr \
 	-l SimpleMath -v 1.0.0 \
-	-nq \
-	--dfa \
+	-nqD \
 	"$script_path/simplemath.md" || { echo "FAIL" >&2 ; exit 1 ; }
 echo "(done)"
 
 echo "[3/4] Output LALR(1) DFA:"
 ./ictcc --lalr \
 	-l SimpleMath -v 1.0.0 \
-	-nq \
-	--dfa \
+	-nqD \
 	"$script_path/simplemath.md" || { echo "FAIL" >&2 ; exit 1 ; }
 echo "(done)"
 
 echo "[4/4] Output LL(1) DFA:"
 ./ictcc --ll \
 	-l SimpleMath -v 1.0.0 \
-	-nq \
-	--dfa \
+	-nqD \
 	"$script_path/simplemath-ll.md" || { echo "FAIL" >&2 ; exit 1 ; }
 echo "(done)"
