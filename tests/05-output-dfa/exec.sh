fcbdc46862e0ba1c6884ccbc7f56b09d3ecb81af
Deka Jello <dekarrin.irl@gmail.com>
2023-05-05 10:59:30 -0500

    #23: Oh. Good. LL parser has no DFA output. What?!

--- tests/05-output-dfa/exec.sh
+++ tests/05-output-dfa/exec.sh
@@ -2,7 +2,7 @@
 
 script_path="$(cd "$(dirname "$0")" >/dev/null ; pwd -P)"
 
-echo "[1/1] Output CLR(1) DFA:"
+echo "[1/4] Output CLR(1) DFA:"
 ./ictcc --clr \
 	-l SimpleMath -v 1.0.0 \
 	-nq \
@@ -10,7 +10,7 @@ echo "[1/1] Output CLR(1) DFA:"
 	"$script_path/simplemath.md" || { echo "FAIL" >&2 ; exit 1 ; }
 echo "(done)"
 
-echo "[2/1] Output SLR(1) DFA:"
+echo "[2/4] Output SLR(1) DFA:"
 ./ictcc --slr \
 	-l SimpleMath -v 1.0.0 \
 	-nq \
@@ -18,10 +18,18 @@ echo "[2/1] Output SLR(1) DFA:"
 	"$script_path/simplemath.md" || { echo "FAIL" >&2 ; exit 1 ; }
 echo "(done)"
 
-echo "[3/1] Output LL(1) DFA:"
-./ictcc --ll \
+echo "[3/4] Output LALR(1) DFA:"
+./ictcc --lalr \
 	-l SimpleMath -v 1.0.0 \
 	-nq \
 	--dfa \
 	"$script_path/simplemath.md" || { echo "FAIL" >&2 ; exit 1 ; }
+echo "(done)"
+
+echo "[4/4] Output LL(1) DFA:"
+./ictcc --ll \
+	-l SimpleMath -v 1.0.0 \
+	-nq \
+	--dfa \
+	"$script_path/simplemath-ll.md" || { echo "FAIL" >&2 ; exit 1 ; }
 echo "(done)"
