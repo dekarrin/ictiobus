7b4cb1215fdb7ef39940ae447c0a080fae5a933c
Rebecca Nelson <dekarrin.irl@gmail.com>
2023-05-05 17:09:01 -0500

    fix go-staticcheck errors

--- grammar/reachability.go
+++ grammar/reachability.go
@@ -2,11 +2,10 @@ package grammar
 
 import (
 	"github.com/dekarrin/ictiobus/internal/box"
-	. "github.com/dekarrin/ictiobus/internal/box"
 	"github.com/dekarrin/ictiobus/internal/slices"
 )
 
-func (g Grammar) ReachableFrom(start string, end string) (bool, []Pair[string, Production]) {
+func (g Grammar) ReachableFrom(start string, end string) (bool, []box.Pair[string, Production]) {
 	if !g.IsNonTerminal(start) {
 		return false, nil
 	}
@@ -17,13 +16,13 @@ func (g Grammar) ReachableFrom(start string, end string) (bool, []Pair[string, P
 	// run reachability algorithm, but instead of starting at the start symbol,
 	// start with each production of it.
 
-	reached := box.NewSVSet[slices.LList[Pair[string, Production]]]()
+	reached := box.NewSVSet[slices.LList[box.Pair[string, Production]]]()
 
 	r := g.Rule(start)
 	for _, p := range r.Productions {
 		for _, sym := range p {
-			var path slices.LList[Pair[string, Production]]
-			path = path.Add(PairOf(start, p))
+			var path slices.LList[box.Pair[string, Production]]
+			path = path.Add(box.PairOf(start, p))
 
 			// if path is any of the not-via's, skip it.
 
@@ -47,7 +46,7 @@ func (g Grammar) ReachableFrom(start string, end string) (bool, []Pair[string, P
 			for _, prod := range rule.Productions {
 				for _, sym := range prod {
 					var path = reached.Get(k)
-					path = path.Add(PairOf(k, prod))
+					path = path.Add(box.PairOf(k, prod))
 
 					if sym == end {
 						return true, path.Slice()
