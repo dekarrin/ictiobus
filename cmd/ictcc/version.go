56c9b12ae20f758c55d5b20724c5b7e0f5975690
Deka Jello <dekarrin.irl@gmail.com>
2023-06-05 12:41:59 -0500

    bumped version number to 1.0.0

--- cmd/ictcc/version.go
+++ cmd/ictcc/version.go
@@ -2,7 +2,7 @@ package main
 
 const (
 	// Version is the version of this release of ictiobus.
-	Version = "0.8.0+dev"
+	Version = "1.0.0"
 )
 
 // GetVersionString returns the current version of ictcc, styled with the name
